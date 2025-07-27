ef34b1f3876680d2bcfeb2b3c068a57046909c3e
sajedur22 <151081528+sajedur22@users.noreply.github.com>
2025-07-27 13:16:27 +0600

    buld

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../../redux/state/counter/counterslice';
+import { increment, decrement, setCustom } from '../../redux/state/counter/counterslice';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   console.log(count) //  Works because reducer name is 'counter'
   const dispatch = useDispatch();
+  const myNumber=useRef()
+   const handleSetCustom = () => {
+    dispatch(setCustom(myNumber.current.value));
+    myNumber.current.value = ''; // <-- input field clear হচ্ছে এখানে
+  };
 
   return (
     <div className="card">
@@ -22,6 +27,14 @@ const Counter = () => {
       <button className="btn btn-danger m-2" onClick={() => dispatch(decrement())}>
         ➖ Decrement
       </button>
+            </div>
+
+            {/*setCustom */}
+            <div className='m-2'>
+              <input className='form-control my-1 border-black w-75' ref={myNumber} type='number'/>
+              <button className="btn btn-danger m-2" onClick={handleSetCustom}>
+              set custom
+             </button>
             </div>
         </div>
       
--- src/redux/state/counter/counterslice.js
+++ src/redux/state/counter/counterslice.js
@@ -14,9 +14,13 @@ reducers:{
     decrement:(state)=>{
         state.value=state.value-1;
     },
+    setCustom:(state,action)=>{
+       
+        state.value=action.payload;
+    },
 }
 
 })
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement,setCustom } = counterSlice.actions;
 export default counterSlice.reducer; 
